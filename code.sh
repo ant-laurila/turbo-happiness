a9c5a022586fce19c15ec908909b3d8baee32257
Antti Laurila <antti.laurila@uef.fi>
2016-10-19 09:51:03 +0300

    matematiikka tehtävä rakennettu

--- code.sh
+++ code.sh
@@ -1,10 +1,85 @@
 # testing only
 
-clear
-echo "Hello all the great people!\n"
-echo "Is this getting more complex?"
 
-for ((i=0;i<10;i++)) 
+
+function getR() {
+return $(( RANDOM % (10 - 5 + 5 ) + 1 ))
+
+}
+
+function makeC() {
+getR
+n=$?
+getR
+i=$?
+
+
+clear
+echo "Paljon on $n + $i?"
+read answ
+corr=$(expr $n + $i)
+if [ "$answ" = "$corr" ]; then
+ echo "Oikein"
+else 
+ echo "Väärin"
+echo "Oikea vastaus: $corr"
+
+fi
+
+}
+
+
+function drawDeco() {
+
+for i in {1..20} 
 do
-echo "I'm with $i\n"
+  echo -n "- "
+  sleep .05
 done
+
+printf "\n"
+
+}
+
+
+
+clear
+echo "Käynnistetään lasku-robottia!"
+drawDeco
+echo "Matematiikka harjoitus Veikolle!"
+echo "Oletko valmis harjoituksiin?"
+
+# getR
+# ran=$?
+# echo "Paljon on $ran+4?"
+
+
+echo "Jos olet valmis, kirjoita k"
+read answer
+
+if [ "$answer" = "k" ]; then
+  echo "--------------------------------"
+  echo "Vastasit: $answer"
+  echo "Aloitetaan, kirjoita ensin nimesi"
+  read player
+  echo "Hei $player! Tervetuloa!"
+  sleep 1
+
+  makeC
+
+  echo "--------------------------------"
+  echo "Haluatko uuden tehtävän? Kirjoita k jos haluat, ja e jos et halua"
+  read new
+  while [ "$new" = "k" ]
+  do
+    makeC
+    echo "Haluatko uuden tehtävän? Kirjoita k jos haluat, ja e jos et halua"
+    read new
+  done
+
+
+else
+  echo "--------------------------------"
+  echo "Etkö halunnutkaan kokeilla?"
+  echo "Ohjelma-robotti keskeytetään"
+fi
